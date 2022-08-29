38e236d4b19d2b0534000bd1144682dcc16a3202
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-08-29 20:56:41 +0200

    category array []

---
+++ src/utils/catergory.ts
@@ -0,0 +1,9 @@
+import {Categories} from "../types";
+
+export const categories: Categories[] = [
+    Categories.All,
+    Categories.Burger,
+    Categories.Drink,
+    Categories.Pizza,
+    Categories.Steak
+]
